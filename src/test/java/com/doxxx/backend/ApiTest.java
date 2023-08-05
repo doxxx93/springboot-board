cd1da43467749848915ca76566b804c78bb6edcf
doxxx <doxxx93@gmail.com>
2023-08-05 13:02:31 +0900

    refactor: convert DirtiesContext to pojo

--- src/test/java/com/doxxx/backend/ApiTest.java
+++ src/test/java/com/doxxx/backend/ApiTest.java
@@ -1,15 +1,26 @@
 package com.doxxx.backend;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 
-@Retention(RetentionPolicy.RUNTIME)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public @interface ApiTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApiTest {
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
 
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+            databaseCleanup.afterPropertiesSet();
+        }
+        databaseCleanup.execute();
+    }
 }
